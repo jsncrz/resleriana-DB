e47c48e3bb66728d70685205abf1279beeab398f
jsncrz <jasoon.acruz@gmail.com>
2025-04-05 17:44:10 -0700

    Add character resist API

---
+++ src/main/java/com/jcruz/reslerianadb/infrastructure/repository/CharacterResistRepository.java
@@ -0,0 +1,9 @@
+package com.jcruz.reslerianadb.infrastructure.repository;
+
+
+import com.jcruz.reslerianadb.infrastructure.entity.CharacterResist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterResistRepository extends JpaRepository<CharacterResist, Integer> {
+
+}
